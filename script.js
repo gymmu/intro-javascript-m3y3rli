8cb68da43abdec29a9b22c8a684b46c84b49c005
Linus Meyer <e227436@sbl.ch>
2023-10-17 11:24:33 +0200

    Aufgabe 1 bis 5(Aufgabe 5  noch nicht ganz fertig)

--- script.js
+++ script.js
@@ -4,5 +4,21 @@ function sayHello() {
 
 function changeBackground() {
   const body = document.querySelector("body")
-  body.style.backgroundColor = "darkgrey"
+  body.style.backgroundColor = "purple"
+}
+
+function deleteText() {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
+}
+
+function decrementCounter() {
+  
 }
