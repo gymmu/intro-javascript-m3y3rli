2962eed138498046ac2eaae2d66d2cd21d336a12
Linus Meyer <e227436@sbl.ch>
2023-10-20 08:06:48 +0200

    Aufgabe 5

--- script.js
+++ script.js
@@ -20,5 +20,8 @@ function incrementCounter() {
 }
 
 function decrementCounter() {
-  
+    const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num
 }
